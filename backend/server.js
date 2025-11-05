eea16621f3805d736827129d58de794d9560e511
SauravKumar04 <2023ugee038@nitjsr.ac.in>
2025-11-05 23:39:53 +0530

    Ready for production

--- backend/server.js
+++ backend/server.js
@@ -39,7 +39,8 @@ app.use(cors({
     "http://localhost:5174", 
     "http://localhost:5175",
     "http://localhost:5176",
-    "http://localhost:3000"
+    "http://localhost:3000",
+    "https://story-pad-26tm.vercel.app"
   ],
   credentials: true
 }));
